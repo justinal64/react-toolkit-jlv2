1fbf046a5240eb09e102e0372ff06d4801962b04
justin leggett <justin@codingrookie.com>
2018-08-03 22:30:05 -0500

    added hamburger for smaller screens

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { A } from "../Elements/Elements";
 import "./Navigation.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDown, faBars } from "@fortawesome/free-solid-svg-icons";
 
 export interface Props {
   title: string;
@@ -44,25 +44,45 @@ export const Navigation = (props: Props) => {
           <Li>
             <A href="http://www.google.com">Google</A>
           </Li>
-          <div className="dropdown">
-            <A href="http://www.google.com">
-              Google
-              <span className="icon-spacing">
-                <FontAwesomeIcon icon={faAngleDown} />
-              </span>
-            </A>
-            <div className="dropdown-content">
-              <div className="dropdown-hover">
-                <A href="http://www.google.com">Link 1</A>
-              </div>
-              <div className="dropdown-hover">
-                <A href="http://www.google.com">Link 2</A>
-              </div>
-              <div className="dropdown-hover">
-                <A href="http://www.google.com">Link 3</A>
+          <Li>
+            <div className="dropdown">
+              <A href="http://www.google.com">
+                Google
+                <span className="icon-spacing">
+                  <FontAwesomeIcon icon={faAngleDown} />
+                </span>
+              </A>
+              <div className="dropdown-content">
+                <div className="dropdown-hover">
+                  <A href="http://www.google.com">Link 1</A>
+                </div>
+                <div className="dropdown-hover">
+                  <A href="http://www.google.com">Link 2</A>
+                </div>
+                <div className="dropdown-hover">
+                  <A href="http://www.google.com">Link 3</A>
+                </div>
               </div>
             </div>
-          </div>
+          </Li>
+          <Li>
+            <div className="dropdown hamburger">
+              <span className="icon-spacing hamburger-icon">
+                <FontAwesomeIcon icon={faBars} />
+              </span>
+              <div className="dropdown-content">
+                <div className="dropdown-hover">
+                  <A href="http://www.google.com">Link 1</A>
+                </div>
+                <div className="dropdown-hover">
+                  <A href="http://www.google.com">Link 2</A>
+                </div>
+                <div className="dropdown-hover">
+                  <A href="http://www.google.com">Link 3</A>
+                </div>
+              </div>
+            </div>
+          </Li>
         </ul>
       </Div>
     </StyledNav>
