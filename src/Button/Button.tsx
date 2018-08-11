54bdca028f824f9cbb1b1848b587d0521ac074da
justin leggett <justin@codingrookie.com>
2018-08-11 00:12:36 -0500

    fixed a few broken links. Still working on @storybook/addon-info

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import styled from "styled-components";
-import themePicker = require("../utils");
+import { themePicker } from "../utils";
 // import {
 //   InvertedPrimaryButton,
 //   InvertedSecondaryButton,
--- src/utils.tsx
+++ src/utils.tsx
@@ -1,3 +1,4 @@
+// change back to .js to fix bigger prob
 import { withInfo } from "@storybook/addon-info";
 import {
   InvertedPrimaryButton,
@@ -45,15 +46,16 @@ opacity: .65;
 pointer-events: none;
 `;
 
-export const getFontSize = props =>
+export const getFontSize = (props: any) =>
   props.font ? `font-size: ${props.font}rem;` : "";
 
-export const getColor = props => (props.color ? `color: ${props.color};` : "");
+export const getColor = (props: any) =>
+  props.color ? `color: ${props.color};` : "";
 
-export const wInfo = text =>
+export const wInfo = (text: any) =>
   withInfo({ inline: true, source: false, styles: wInfoStyle, text: text });
 
-const invertedButton = button => {
+export const invertedButton = (button: any) => {
   if (button === "Primary") {
     return InvertedPrimaryButton;
   } else if (button === "Secondary") {
@@ -69,7 +71,7 @@ const invertedButton = button => {
   }
 };
 
-const buttonPicker = button => {
+export const buttonPicker = (button: any) => {
   if (button === "Primary") {
     return PrimaryButton;
   } else if (button === "Secondary") {
@@ -85,7 +87,23 @@ const buttonPicker = button => {
   }
 };
 
-const themePicker = (inverted, button) => {
+export const invertedButtonPicker = (button: string) => {
+  if (button === "Primary") {
+    return InvertedPrimaryButton;
+  } else if (button === "Secondary") {
+    return InvertedSecondaryButton;
+  } else if (button === "Success") {
+    return InvertedSuccessButton;
+  } else if (button === "Warning") {
+    return InvertedWarningButton;
+  } else if (button === "Danger") {
+    return InvertedDangerButton;
+  } else {
+    return InvertedPrimaryButton;
+  }
+};
+
+export const themePicker = (inverted: boolean, button: any) => {
   return inverted ? invertedButtonPicker(button) : buttonPicker(button);
 };
 
